61f49c949cda61574275bf8c6c6f03b857b17610
Pratik Singh <singhpratik0001@gmail.com>
2023-07-08 23:52:13 +0530

    Leftnav Completed

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import LeftNavMenuItem from './LeftNavMenuItem'
+import { categories } from '../utils/constants'
+import { Context } from '../context/contextApi'
+
 
 const LeftNav = () => {
+
+  const { selectCategories, setSelectCategories, mobileMenu } = useContext(Context)
+  const navigate = useNavigate()
+
+  const clickHandler = (name, type) => {
+    switch (type) {
+      case 'category':
+        return setSelectCategories(name)
+      case 'home':
+        return setSelectCategories(name)
+      case 'menu':
+        return false
+      default:
+        break;
+    }
+  }
+
   return (
-    <div>LeftNav</div>
+    <div className='md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 traslate-x-[240] md:translate-x-0 transition-all'>
+      <div className="flex px-5 flex-col">
+        {categories.map((item) => {
+          return (
+            <>
+              <LeftNavMenuItem
+                text={item.type === 'home' ? 'Home' : item.name}
+                icon={item.icon}
+                action={() => {
+                  clickHandler(item.name, item.type)
+                  navigate('/')
+                }}
+                className={`${selectCategories === item.name ? 'bg-white/[0.15]':''}`}
+              />
+              {item.divider && (
+                <hr className='my-5 border-white/[0.2]'/>
+              )}
+            </>
+          )
+        })}
+        <hr className='my-5 border-white/[0.2]' />
+        <div className='text-white/[0.5] text-[12px]'>
+          Cloned by : Pratik Singh
+        </div>
+      </div>
+    </div>
   )
 }
 
